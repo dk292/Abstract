ae8b18f1832b0702179c80eabc8e802b83e13c0c
Htet Hlaing Win <htethlaingwin.bdm29@gmail.com>
2022-10-24 09:05:06 +0630

    finished project

---
+++ index.js
@@ -0,0 +1,7 @@
+function openNav() {
+    document.getElementById("side_drawer").style.display = "block";
+}
+
+function closeNav() {
+    document.getElementById("side_drawer").style.display = "none";
+}
